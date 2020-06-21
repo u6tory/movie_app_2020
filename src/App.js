e4b3b77d83588bf4e8bbb9805850b52cf807697f
u6tory <30873030+u6tory@users.noreply.github.com>
2020-06-22 02:31:41 +0900

    #4.1 Fetching Movies and rendering the Movies

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import React from "react";
-// import PropTypes from "prop-types";
+import axios from "axios";
+import Movie from "./Movie";
 
 class App extends React.Component {
   state = {
     isLoading: true,
-    book: true,
+    movies: [],
   };
 
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false, movie: [] });
-      console.log(this.state.book);
-      console.log(this.state.movie);
-    }, 5000);
-  }
+  // 1) async + axios를 통해 데이터를 가져오고, setState으로 데이터를 적용한다.
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get( // axios로 fetch한다.
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
+  };
 
+  // 2) Component Mount가 끝나면 미리 정의해둔 (1)함수를 실행한다.
+  componentDidMount = () => this.getMovies();
+
+  // 3) Movie.js에 설정해둔 Function Component로 state의 data를 넣어 mapping한다.
   render() {
-    const { isLoading } = this.state;
-    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
+    const { isLoading, movies } = this.state;
+    return (
+      <div>
+        {" "}
+        {isLoading
+          ? "Loading..."
+          : movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+              />
+            ))}
+      </div>
+    );
   }
 }
+
 export default App;
